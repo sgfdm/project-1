575de3159fa2223a552506d9dcf7a89eba8590bc
sathu-1 <sathusan.ganesh@gmail.com>
2023-03-03 12:38:27 -0500

    Updated ReadMe with more info & completed implementing all features on main.tf

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # project-1
 Terraform Project, (DevOps Week 1)
+
+This repository includes four files (main.tf, providers.tf, variable.tf, output.tf.)
+The purpose of the project is to create and provision cloud infrastructure on AWS and Azure using Terraform. 
+
+AWS resources created are: 4 IAM accounts via for_each, 2 S3 buckets via vars and count.
+
+Azure resources created are: 2 Azure accounts, where 1 is required to reset password. Resource Group, Virtual Machine and Storage Account. 
+
+All provider information is kept in provider.tf. Providers aused are AWS, Azurerm and AzureAD.
+
+All variables are stored within the variable.tf file. 
+
+The code will output the region of the AWS and AZURE servers. 
+
+The AWS IAM Accounts have been tagged as "trainees", for cost tracking reasons. 
+
+The Azure Storage Account has been tagged as "DEV" as it will be used for Dev projects.  
--- main.tf
+++ main.tf
@@ -16,30 +16,20 @@ terraform {
   
 }
 
+resource "azurerm_resource_group" "rg-project1" {
+  name     = var.az-rg-name
+  location = var.az-location
+}
 
 resource "azuread_user" "create_user_sathu" {
   user_principal_name = "s.ganesathasan@fdmgroup.com"
-  display_name = "Sathu"
-
- 
-  
+  display_name = "Sathu" 
 }
 
 resource "azuread_user" "create_user_ibrahim" {
   user_principal_name = "Sathusan.Ganesathasan@fdmgroup.com"
   display_name = "Ibrahim"
-  force_password_change = true
-
-   
-}
-
-
-resource "aws_iam_user" "new-users"{
-    for_each = toset(var.aws-users-names)
-    name = each.value
-     tags = {
-    user-type = "trainee"
-  }
+  force_password_change = true   
 }
 
 resource "aws_s3_bucket" "two_buckets_creation"{
@@ -47,16 +37,12 @@ resource "aws_s3_bucket" "two_buckets_creation"{
     count = var.number_of_buckets
 }
 
-resource "azurerm_resource_group" "rg-project1" {
-  name     = var.az-rg-name
-  location = var.az-location
-}
-
-resource "azurerm_virtual_network" "vm-project1" {
-  name                = "sathu-az-vm-project1"
-  address_space       = ["10.0.0.0/16"]
-  location            = var.az-location
-  resource_group_name = var.az-rg-name
+resource "aws_iam_user" "new-users"{
+    for_each = toset(var.aws-users-names)
+    name = each.value
+     tags = {
+    user-type = "trainee"
+  }
 }
 
 resource "azurerm_storage_account" "storage-account-project1" {
@@ -68,5 +54,59 @@ resource "azurerm_storage_account" "storage-account-project1" {
 
   tags = {
     environment = "dev"
+  }
+}
+
+/* 
+    Creating AZ Linux VM 
+*/
+
+resource "azurerm_virtual_network" "vm-project1" {
+  name                = "sathu-az-vm-project1"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.az-location
+  resource_group_name = var.az-rg-name
+}
+
+resource "azurerm_subnet" "subnet-project1" {
+  name                 = "sathu-az-subnet-project1"
+  resource_group_name  = var.az-rg-name
+  virtual_network_name = azurerm_virtual_network.vm-project1.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "nic-project1" {
+  name                = "sathu-az-nic-project1"
+  location            = var.az-location
+  resource_group_name = var.az-rg-name
+
+  ip_configuration {
+    name                          = "sathu-az-ipconfig-project1"
+    subnet_id                     = azurerm_subnet.subnet-project1.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "vm-project1" {
+  name                = "sathu-az-vm-project1"
+  resource_group_name = var.az-rg-name
+  location            = var.az-location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.nic-project1.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
   }
 }
